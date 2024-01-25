612ce387ab800a03c27a25315b7e99d45ac33f9f
ManishNagesh <dev.manishn@gmail.com>
2024-01-25 23:05:13 +0530

    part3-d

--- mongo.js
+++ mongo.js
@@ -21,11 +21,11 @@ const noteSchema = new mongoose.Schema({
 const Note = mongoose.model('Note', noteSchema)
 
 const note = new Note({
-  content: 'Lambu ki mkb',
+  content: 'Nothing',
   important: false,
 })
 
-note.save().then(result => {
+note.save().then(() => {
   console.log('note saved!')
   mongoose.connection.close()
 })
